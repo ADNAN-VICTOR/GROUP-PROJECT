48ad291eb3eb1d5a7be45059f84a4363e6cc861a
Joy Renee Mugo <mugojoy24@gmail.com>
2024-05-07 17:45:52 +0300

    search component

--- recipe-app/src/components/SearchBar.js
+++ recipe-app/src/components/SearchBar.js
@@ -1,1 +1,33 @@
 import React from "react";
+
+
+
+function SearchBar() {
+    return(
+        <div>
+            <header>
+                <form className="search-form">
+                    <div>
+                        <h2 className="recipeDiv">Recipe App</h2>
+                    </div>
+                    <div className="div-input"> 
+                        <input 
+                            className="search-input"
+                            type="search"
+                            placeholder="Search"
+                        />
+                        
+                            
+                        
+                        
+                    </div>
+                </form> 
+            </header>
+            
+        </div>
+        
+    )
+    
+}
+
+export default SearchBar
