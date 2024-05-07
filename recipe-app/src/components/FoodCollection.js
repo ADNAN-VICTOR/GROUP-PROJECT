9abdf6f8d146c2f391741734607081dba0e36de3
adnan <adnan.victor@student.moringaschool.com>
2024-05-07 19:16:04 +0300

    foodcollection

--- recipe-app/src/components/FoodCollection.js
+++ recipe-app/src/components/FoodCollection.js
@@ -1,1 +1,21 @@
 import React from "react";
+
+function FoodCollection() {
+    return (
+      //map data here
+      <div>
+        <h1>Food Collection</h1>
+        <div id="FoodContainer" className="bg bg-info-subtle"> 
+            <div className="bg bg-secondary-subtle m-3">
+              <img src="linkplace holder" alt="food img placeholder" />
+              <h2>Food Name Placeholder</h2>
+              <p>Calories:</p>
+              <p>Food Type:</p>
+              <button className="btn btn-info">View Recipe</button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default FoodCollection;
