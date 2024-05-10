d72cef204e424ac02fe5874af82d40249adf5773
patricia <patricia.mwangi@student.moringaschool.com>
2024-05-10 16:46:54 +0300

    form function

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import FoodCollection from './components/FoodCollection';
+import {FoodCollection} from './components/FoodCollection';
 import SearchBar from './components/SearchBar';
 import { Form } from './components/Navbar';
+import React,{useState,useEffect} from 'react';
 function App() {
+  const [foods, setFoods] = useState([])
+  useEffect(() => { 
+    fetch("https://json-recipe.onrender.com/food")
+    .then(res => res.json())
+    .then(data => {setFoods(data); console.log(data)})
+    .catch(error => console.error("Error fetching Data!!!", error))
+    
+  }, [])
+
   return (
     <div>
       
       <SearchBar />
-      <Form/>
-      <FoodCollection/>
+      <Form foods={foods}/>
+      <FoodCollection foods={foods}/>
     </div>
   );
 }
--- recipe-app/src/components/Navbar.js
+++ recipe-app/src/components/Navbar.js
@@ -1,22 +1,80 @@
-import React from "react";
-export function Form(){
+import React, { useState} from "react";
+export function Form({foods}){
+const [category,setCategory]=useState({foods})
+const[view,setView]=useState()
+console.log(category,'c')
+let change =(type)=>{
+       setCategory(prevState=>prevState===type? true:type)
+}
+ function viewed(f){
+    console.log(view,'v')
+setView(prevState=>prevState===f? null:f)
+ }
+let filtered= foods.filter((f)=>f.food_type===category)
     return(
+        <>
         <div className="">
-        <form id="form">
-        <button name="All" >Delicacies</button>
-        <input type="checkbox" name="vegeterian" id="vegeterian"/>
-        <label for="vegeterian">Vegeterian</label>
-        <input type="checkbox" name="seafood" id="seafood"/>
-        <label for="seafood">Seafood</label>
-        <input type="checkbox" name="pork" id="pork"/>
-        <label for="pork">Pork</label>
-        <input type="checkbox" name="beef" id="beef"/>
-        <label for="beef">Beef</label>
-        <input type="checkbox" name="lamb" id="lamb"/>
-        <label for="lamb">Lamb</label>
-        <input type="checkbox" name="chicken" id="chicken"/>
-        <label for="chicken">Chicken</label>
+        <form id="form" onSubmit={(e)=>e.preventDefault()}>
+        <input 
+        type="checkbox"
+         name="vegeterian" 
+         onChange={()=>change('vegetarian')}
+         checked={category==='vegetarian'}
+         id="vegeterian"
+         />
+        <label htmlFor="vegeterian">Vegetarian</label>
+        <input type="checkbox"
+         name="seafood" 
+         onChange={()=>change('seafood')}
+          checked={category==='seafood'}
+          id="seafood"/>
+        <label htmlFor="seafood">Seafood</label>
+        <input type="checkbox"
+         name="pork" 
+         onChange={()=>change('pork')}
+         checked={category==='pork'}
+         id="pork"/>
+        <label htmlFor="pork">Pork</label>
+        <input type="checkbox" 
+        name="beef" 
+        onChange={()=>change('beef')}
+        checked={category==='beef'}
+        id="beef"/>
+        <label htmlFor="beef">Beef</label>
+        <input type="checkbox" 
+        name="lamb" 
+        onChange={()=>change('lamb')}
+        checked={category==='lamb'}
+        id="lamb"/>
+        <label htmlFor="lamb">Lamb</label>
+        <input type="checkbox"
+         name="chicken"        
+         onChange={()=>change('chicken')}
+         checked={category==='chicken'}
+
+         id="chicken"/>
+        <label htmlFor="chicken">Chicken</label>
         </form>
         </div>
+        <ul>
+            {filtered.map((m)=>(
+                    <div key={m.id} style={{ display: 'flex' }}>
+                    <img src={m.food_pic} alt="food img placeholder" />
+                    <div style={{marginLeft:"10px"}}>
+                 <h2>{m.name}</h2>
+                 <p>Calories: {m.calories}</p>
+                 <p>Food Type: {m.food_type}</p>
+                 <button onClick={()=>viewed(m)}>{view===m ?'Close Recipe':"View Recipe"}</button><br></br>
+                
+                 </div>
+                 </div>
+            ))}
+        </ul>
+        {view &&(
+            <div style={{marginLeft:'10px'}}>
+            <h3 >{view.recipe}</h3>
+            </div>
+        )}
+        </>
     )
 }
